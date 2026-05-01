c1850ba32c985ef9d5863dee385c97ab4502c69c
shrutising6226-star <shrutising6226@gmail.com>
2026-05-02 01:39:47 +0530

    Universal Express 5 routing fix

--- server/index.js
+++ server/index.js
@@ -299,7 +299,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   
-  app.get('/:any*', (req, res) => {
+  app.get('/:path([\\s\\S]*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 } else {
